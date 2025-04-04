a2c6af0ad67b9861dc479849a23490cff9099406
Divya Prakash <rkisdp@gmail.com>
2025-04-05 00:33:26 +0530

    chore(article): Added new medium article.

--- src/components/sections/ArticlesSection.vue
+++ src/components/sections/ArticlesSection.vue
@@ -159,9 +159,16 @@ const articles: Article[] = [
   {
     title: "Why Type Checking is a Game-Changer for Python Projects",
     url: "https://rkisdp.medium.com/why-type-checking-is-a-game-changer-for-python-projects-and-how-it-will-save-your-lots-of-time-a10d656d2257",
-    date: "Mar 14, 2024",
+    date: "Mar 14, 2025",
     image:
       "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*ma0Ee8bOoLhyuBDLua0tYQ.jpeg",
+  },
+  {
+    title: "A Guide To Understanding Django REST Framework Views",
+    url: "https://rkisdp.medium.com/guide-of-understanding-django-rest-framework-views-940853a5964f",
+    date: "April 5, 2025",
+    image:
+        "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*70lDa3cF_vQDWuVY8eAZBQ.png",
   },
 ];
 
