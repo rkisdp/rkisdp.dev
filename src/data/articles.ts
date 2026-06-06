0b49ce74751baa48dd1b196b56f4dde090eb722d
Divya Prakash <rkisdp@gmail.com>
2026-06-06 14:55:07 +0530

    added medium article

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -1,30 +1,37 @@
 export interface Article {
-  title: string;
-  url: string;
-  date: string;
-  image: string;
+    title: string;
+    url: string;
+    date: string;
+    image: string;
 }
 
 export const articles: Article[] = [
-  {
+    {
     title: "Why Type Checking is a Game-Changer for Python Projects",
     url: "https://rkisdp.medium.com/why-type-checking-is-a-game-changer-for-python-projects-and-how-it-will-save-your-lots-of-time-a10d656d2257",
     date: "Mar 14, 2025",
     image:
       "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*ma0Ee8bOoLhyuBDLua0tYQ.jpeg",
-  },
-  {
+    },
+    {
     title: "A Guide To Understanding Django REST Framework Views",
     url: "https://rkisdp.medium.com/guide-of-understanding-django-rest-framework-views-940853a5964f",
     date: "April 5, 2025",
     image:
         "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*70lDa3cF_vQDWuVY8eAZBQ.png",
-  },
-  {
+    },
+    {
     title: "Async Programming In Python: Boost Performance and Efficiency of Your Code",
     url: "https://rkisdp.medium.com/async-programming-in-python-boost-performance-and-efficiency-of-your-code-98f1bd29c237",
     date: "August 13, 2025",
     image:
         "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*wZijDlLHoigao77smKm9Hw.png",
-  },
+    },
+    {
+        title: "How I Reduced CI Runtime for My Django Project with 4200+ Tests from 50–55 Minutes to Just 17 Minutes in GitHub Actions",
+        url: "https://rkisdp.medium.com/how-i-reduced-ci-runtime-for-my-django-project-with-4200-tests-from-50-55-minutes-to-just-17-9f6819e348d9",
+        date: "June 6, 2026",
+        image:
+            "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*iyLLo9F9EI5Hnqh5zNLJxA.png",
+    },
 ];
